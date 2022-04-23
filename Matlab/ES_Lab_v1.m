ec8666e32be38e8516f00cfb0fc386c28e63121c
Ban <banhanyuan@megvii.com>
2022-04-23 19:19:29 +0200

    PC2D

---
+++ Matlab/ES_Lab_v1.m
@@ -0,0 +1,176 @@
+% ES labs 2021-2022
+% Author: Kenrick Trip 
+
+clear all
+
+%% All variables:
+l = 0.26; % m
+m1 = 0.7; % kg
+m2 = 0.08; % kg
+r1 = 0.045; % m
+r2 = l/sqrt(2); % m
+
+Iy = (2*m1*r1^2)/5 + 4*m2*l^2; % yaw inertia
+Ip = (2*m1*r1^2)/5 + 4*m2*r2^2; % pitch inertia
+Ir = Ip; % roll inertia
+
+% PWM to thrust (T) and torque (Q):
+cT = (0.78*9.81)/255;
+cQ = 0.07/255;
+
+% period of microcontroller
+dt = 0.1; % s
+
+%% Model:
+
+% construct state space model
+A = [0,0,0,1,0,0;
+     0,0,0,0,1,0;
+     0,0,0,0,0,1;
+     zeros(3,6)];
+
+B = [zeros(3,4);
+     (cQ/Iy)*[-1,1,1,-1];
+     ((cT*l)/(Ip*sqrt(2)))*[1,1,-1,-1];
+     ((cT*l)/(Ir*sqrt(2)))*[1,-1,1,-1]];
+
+C = [eye(3),zeros(3,3)];
+
+D = 0;
+
+% open loop Continuous Time system:
+OLsys = ss(A,B,C,D);
+
+OLsys.Inputname = {'s1','s2','s3','s4'};
+OLsys.Outputname = {'y','p','r'};
+
+%% Controller:
+
+% Closed loop control
+tau = 0.1;
+
+Kpy = 18.5; % controller P gain yaw
+Kiy = 1.07; % controller I gain yaw
+Kdy = 79.8; % controller D gain yaw
+
+Kpp = 0.471; % controller P gain pitch
+Kip = 0.0273; % controller I gain pitch
+Kdp = 2.04; % controller D gain pitch
+
+Kpr = 0.471; % controller P gain roll
+Kir = 0.0273; % controller I gain roll
+Kdr = 2.04; % controller D gain roll
+
+s = tf('s');
+Cy = (Kpy + Kiy/s + Kdy*s)/(tau*s+1); % yaw controller
+Cp = (Kpp + Kip/s + Kdp*s)/(tau*s+1); % pitch controller
+Cr = (Kpr + Kir/s + Kdr*s)/(tau*s+1); % roll controller
+
+% test PID autotune:
+% [C_y1,info] = pidtune(OLsys(1,1),'PID')
+% [C_y2,info] = pidtune(OLsys(1,2),'PID')
+% [C_y3,info] = pidtune(OLsys(1,3),'PID')
+% [C_y4,info] = pidtune(OLsys(1,4),'PID')
+% [C_p1,info] = pidtune(OLsys(2,1),'PID')
+% [C_p2,info] = pidtune(OLsys(2,2),'PID')
+% [C_p3,info] = pidtune(OLsys(2,3),'PID')
+% [C_p4,info] = pidtune(OLsys(2,4),'PID')
+% [C_r1,info] = pidtune(OLsys(3,1),'PID')
+% [C_r2,info] = pidtune(OLsys(3,2),'PID')
+% [C_r3,info] = pidtune(OLsys(3,3),'PID')
+% [C_r4,info] = pidtune(OLsys(3,4),'PID')
+
+Csys = [-Cy, Cp, Cr;
+        Cy, Cp, -Cr; 
+        Cy, -Cp, Cr; 
+        -Cy, -Cp, -Cr];
+
+Csys.Inputname = {'err_y', 'err_p', 'err_r'};
+Csys.Outputname = {'s1','s2','s3','s4'};
+
+Sum1 = sumblk('err_y = y_{ref} - y');
+Sum2 = sumblk('err_p = p_{ref} - p');
+Sum3 = sumblk('err_r = r_{ref} - r');
+
+CLsys = connect(OLsys,Csys,Sum1,Sum2,Sum3,{'y_{ref}','p_{ref}','r_{ref}'},...
+               {'y', 'p', 'r'});
+p = pole(CLsys);
+
+% discretise controller
+DT_CLsys = c2d(CLsys,dt,'tustin');
+DT_Csys = c2d(Csys,dt,'tustin');
+DT_OLsys = c2d(OLsys,dt,'tustin');
+
+%% Simulations
+
+% step response system
+figure(1)
+step(DT_CLsys, 3);
+
+% simulate input control sequences:
+t = 0:dt:15;
+y_ref = 0.1*square(t);
+p_ref = -0.2*square(t-1);
+r_ref = 0.2*square(t-0.5); %zeros(1,length(t));
+u = [y_ref; p_ref; r_ref];
+
+y = lsim(DT_CLsys,u,t);
+mot_sig = lsim(DT_Csys,u-y',t);
+
+% add throttle and saturation
+throttle = 200; % between 0 and 255
+
+ESC_unsat = mot_sig + throttle*ones(length(t),4);
+
+ESC_sat = zeros(length(t),4);
+
+UB = 255-throttle;
+LB = -UB;
+
+for i = 1:length(t)
+    for k = 1:4
+        ESC_sat(i,k) = throttle + min(UB, max(LB, mot_sig(i,k)));
+    end
+end
+
+y_sat = lsim(DT_OLsys,mot_sig,t);
+
+% plot reference tracking
+figure(2)
+stairs(t,y)
+hold on
+plot(t,u)
+hold off
+title('I/O')
+legend('y','p','r','y_{ref}','p_{ref}','r_{ref}')
+ylabel('Angles (rad)')
+xlabel('Time (s)')
+
+% plot ESC signals
+figure(3)
+stairs(t,mot_sig)
+title('ESC signals without throttle or saturation')
+legend('s1','s2','s3','s4')
+ylabel('PWM signal \pm(0,255)')
+xlabel('Time (s)')
+
+% plot ESC signals
+figure(4)
+stairs(t,ESC_sat)
+title('ESC signals with throttle and saturation')
+legend('s1','s2','s3','s4')
+ylabel('PWM signal \pm(0,255)')
+xlabel('Time (s)')
+
+% plot reference tracking
+figure(5)
+stairs(t,y_sat)
+hold on
+plot(t,u)
+hold off
+title('I/O')
+legend('y','p','r','y_{ref}','p_{ref}','r_{ref}')
+ylabel('Angles (rad)')
+xlabel('Time (s)')
+
+
---
+++ fcb_software-master/in4073/in4073.c
@@ -0,0 +1,175 @@
+/*------------------------------------------------------------------
+ *  in4073.c -- test QR engines and sensors
+ *
+ *  reads ae[0-3] uart rx queue
+ *  (q,w,e,r increment, a,s,d,f decrement)
+ *
+ *  prints timestamp, ae[0-3], sensors to uart tx queue
+ *
+ *  I. Protonotarios
+ *  Embedded Software Lab
+ *
+ *  June 2016
+ *------------------------------------------------------------------
+ */
+#include "in4073.h"
+#include <stdbool.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include "nrf_gpio.h"
+#include "nrf_delay.h"
+#include "app_util_platform.h"
+#include "adc.h"
+#include "barometer.h"
+#include "gpio.h"
+#include "spi_flash.h"
+#include "timers.h"
+#include "twi.h"
+#include "hal/uart.h"
+#include "control.h"
+#include "mpu6050/mpu6050.h"
+#include "utils/quad_ble.h"
+#include "PC2D.h"
+#include "queue.h"
+
+bool demo_done;
+
+/*------------------------------------------------------------
+ * parse message
+ *------------------------------------------------------------
+ */
+
+int receive_message(PC2D_message_p mes, Queue* q, int len) {
+	for (int i = 0; i < len; i++) {
+		uint8_t val = dequeue(q);
+		if (val == (uint8_t) -1) {
+			printf("wrong at %d\n", i);
+			return -1;
+		}
+		if (i == 0) {
+			mes->checksum = val;
+			if (val != len) {
+				printf("wrong checksum");
+				return -1;
+			}
+		}
+		if (i == 1) mes->mode = val;
+		if (i == 2) mes->control.x = (uint16_t) val;
+		if (i == 3) mes->control.x |= ((uint16_t) val) << 8;
+		if (i == 4) mes->control.y = (uint16_t) val;
+		if (i == 5) mes->control.y |= ((uint16_t) val) << 8;
+		if (i == 6) mes->control.z = (uint16_t) val;
+		if (i == 7) mes->control.z |= ((uint16_t) val) << 8;
+		if (i == 8) mes->key = (char) val;
+	}
+	return 1;
+}
+
+/*------------------------------------------------------------------
+ * process_key -- process command keys
+ *------------------------------------------------------------------
+ */
+void process_key(uint8_t c)
+{
+	switch (c) {
+	case 'q':
+		ae[0] += 10;
+		break;
+	case 'a':
+		ae[0] -= 10;
+		if (ae[0] < 0) ae[0] = 0;
+		break;
+	case 'w':
+		ae[1] += 10;
+		break;
+	case 's':
+		ae[1] -= 10;
+		if (ae[1] < 0) ae[1] = 0;
+		break;
+	case 'e':
+		ae[2] += 10;
+		break;
+	case 'd':
+		ae[2] -= 10;
+		if (ae[2] < 0) ae[2] = 0;
+		break;
+	case 'r':
+		ae[3] += 10;
+		break;
+	case 'f':
+		ae[3] -= 10;
+		if (ae[3] < 0) ae[3] = 0;
+		break;
+	case 27:
+		demo_done = true;
+		break;
+	default:
+		nrf_gpio_pin_toggle(RED);
+	}
+}
+
+
+/*------------------------------------------------------------------
+ * main -- everything you need is here :)
+ *------------------------------------------------------------------
+ */
+int main(void)
+{
+	uart_init();
+	gpio_init();
+	timers_init();
+	adc_init();
+	twi_init();
+	imu_init(true, 100);
+	baro_init();
+	spi_flash_init();
+	quad_ble_init();
+
+	uint32_t counter = 0;
+	demo_done = false;
+	wireless_mode = false;
+
+	PC2D_message rec_mes = create_message();
+
+	while (!demo_done) {
+		if (rx_queue.count) {
+			if (receive_message(&rec_mes, &rx_queue, sizeof(rec_mes)) == 1) {
+				printf("RECEIVED_MESSAGE:\n");
+				printf("checksum: %d\n", rec_mes.checksum);
+				printf("mode: %d\n", rec_mes.mode);
+				printf("controls: %d %d %d\n", rec_mes.control.x, rec_mes.control.y, rec_mes.control.z);
+				printf("key: %c\n", rec_mes.key);
+			}
+		}
+		if (ble_rx_queue.count) {
+			process_key(dequeue(&ble_rx_queue));
+		}
+
+		if (check_timer_flag()) {
+			if (counter++%20 == 0) {
+				nrf_gpio_pin_toggle(BLUE);
+			}
+
+			adc_request_sample();
+			read_baro();
+
+			printf("%10ld | ", get_time_us());
+			printf("%3d %3d %3d %3d | ",ae[0], ae[1], ae[2], ae[3]);
+			printf("%6d %6d %6d | ", phi, theta, psi);
+			printf("%6d %6d %6d | ", sp, sq, sr);
+			printf("%4d | %4ld | %6ld \n", bat_volt, temperature, pressure);
+
+			clear_timer_flag();
+		}
+
+		if (check_sensor_int_flag()) {
+			get_sensor_data();
+			run_filters_and_control();
+		}
+	}	
+
+	printf("\n\t Goodbye \n\n");
+	nrf_delay_ms(100);
+
+	NVIC_SystemReset();
+}
